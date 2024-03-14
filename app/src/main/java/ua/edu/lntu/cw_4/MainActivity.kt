bb3a41266fe582a04b7a887eb3aa7d15b01f8542
praisesun <blackstar934589@gmail.com>
2024-03-14 15:14:19 +0200

    вмикаємо навігацію

--- app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
@@ -3,7 +3,10 @@ package ua.edu.lntu.cw_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,32 +18,9 @@ import ua.edu.lntu.cw_4.ui.theme.IPZ_CW_4_Diak_VladyslavTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            IPZ_CW_4_Diak_VladyslavTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            IPZ_CW_4_Diak_VladyslavTheme { Nav() }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_4_Diak_VladyslavTheme {
-        Greeting("Android")
     }
 }
